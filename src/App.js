a24f85bac28f157a75a52ee92afea7ff68c818d9
Alyssa Vallejo <anvallejo@ucdavis.edu>
2023-01-16 03:08:56 -0800

    fixed a major bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import rat from './rat1.png';
+import cat from './cat.png';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+
+
+function App()
+{
+  const [data, setData] = useState([])
+
+  const fetchData = () => {
+    fetch("https://catfact.ninja/fact")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setData(data)
+      })
+  }
+  
+    return (
+      <div>
+        <header className="App-header">
+        <h1>Welcome to Alyssa's Cat Fact Finder!</h1>
+
+        <img src={cat} className="App-logo" alt="logo" />
+        <p>Click here for your cat fact!</p>
+        <button onClick={fetchData}>Click Me</button>
+        {data.length > 0 && (
+          <ul>
+            {data.fact}
+          </ul>
+        )}
+        </header>
+      </div>
+    )
+  }
+    /*<div className="App">
       <header className="App-header">
-        <img src={rat} className="App-logo" alt="logo" />
-        <p>
-          Uh Oh! Something went terribly wrong! Please pet the rat while you wait for the Mage to fix it.
-        </p>
+        <h1>Welcome to Alyssa's Cat Fact Finder!</h1>
+
+        <img src={cat} className="App-logo" alt="logo" />
+        <p>Click here for your cat fact!</p>
+        <Button 
+        border=""
+        color="lavender"
+        height = "40px"
+        onClick={getData}>Fetch data
+        radius = "5%"
+        width = "100px"
+        children = "Click me!"
+      />
+        
       </header>
-    </div>
-  );
-}
+    </div>*/
+
+
+
+
 
 export default App;
 
